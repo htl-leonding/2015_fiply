4a6783589dfa1d15447c81745e4bd79baf2a54c7
DenkerMayr <andreas.denkmayr@gmail.com>
2015-10-18 19:48:40 +0200

    Willkommen-Nachricht und Navigationsbuttons hinzugefügt.

--- FIPLY/App/app/src/main/java/htl_leonding/fiplyteam/fiply/MainActivity.java
+++ FIPLY/App/app/src/main/java/htl_leonding/fiplyteam/fiply/MainActivity.java
@@ -1,14 +1,22 @@
 package htl_leonding.fiplyteam.fiply;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    Button startUe;
+    Button startTr;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startUe = (Button) findViewById(R.id.btStartUe);
+        startTr = (Button) findViewById(R.id.btStartTr);
     }
+
 
 }
