a9bc6c29dc3196d261ba7942b03af8f7db4afe58
DenkerMayr <andreas.denkmayr@gmail.com>
2015-10-18 20:50:58 +0200

    Navigation zu allen Activitys wurde erstellt

--- FIPLY/App/app/src/main/java/htl_leonding/fiplyteam/fiply/MainActivity.java
+++ FIPLY/App/app/src/main/java/htl_leonding/fiplyteam/fiply/MainActivity.java
@@ -1,21 +1,50 @@
 package htl_leonding.fiplyteam.fiply;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     Button startUe;
-    Button startTr;
+    Button startTS;
+    Button startEU;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startUe = (Button) findViewById(R.id.btStartUe);
-        startTr = (Button) findViewById(R.id.btStartTr);
+        startTS = (Button) findViewById(R.id.btStartTr);
+        startEU = (Button) findViewById(R.id.btStartEU);
+
+        startUe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openUe = new Intent("fiply.UEBUNGSKATALOGACTIVITY");
+                startActivity(openUe);
+            }
+        });
+
+        startTS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openTS = new Intent("fiply.TRAININGSESSIONACTIVITY");
+                startActivity(openTS);
+            }
+        });
+
+        startEU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openEU = new Intent("fiply.ERSTELLEUSERACTIVITY");
+                startActivity(openEU);
+            }
+        });
+
+
+
     }
-
-
 }
