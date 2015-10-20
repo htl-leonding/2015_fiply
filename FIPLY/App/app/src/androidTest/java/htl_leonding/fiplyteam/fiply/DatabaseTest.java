8886a4306f296bb8445344bc9bb9542a8441a8db
DenkerMayr <andreas.denkmayr@gmail.com>
2015-10-20 22:48:26 +0200

    Die ersten 2 Tests wurden ausgiebig getestet

--- FIPLY/App/app/src/androidTest/java/htl_leonding/fiplyteam/fiply/DatabaseTest.java
+++ FIPLY/App/app/src/androidTest/java/htl_leonding/fiplyteam/fiply/DatabaseTest.java
@@ -1,5 +1,6 @@
 package htl_leonding.fiplyteam.fiply;
 
+import android.database.Cursor;
 import android.test.AndroidTestCase;
 import android.test.RenamingDelegatingContext;
 
@@ -11,6 +12,7 @@ public class DatabaseTest extends AndroidTestCase {
         super.setUp();
         RenamingDelegatingContext context = new RenamingDelegatingContext(getContext(), "test_");
         dbA = new DBAdapter(context);
+        dbA.open();
     }
 
     @Override
@@ -19,12 +21,31 @@ public class DatabaseTest extends AndroidTestCase {
         dbA.close();
     }
 
-    public void testOpenDb() {
-        dbA.open();
-    }
-
     public void testAddUebung() {
         dbA.open();
         dbA.insertUebung("Curls", "Mit Gewichten wird gecurlt", "Gewicht nehmen und anschließend curlen", "Bizeps", "Langsam durchführen", "https://www.youtube.com/watch?v=FtAz_85aVxE");
+        Cursor c = dbA.getUebung(1);
+        assertEquals("Curls", c.getString(1));
+    }
+
+    public void testGetAll() {
+        dbA.open();
+        dbA.insertUebung("Curls", "Mit Gewichten wird gecurlt", "Gewicht nehmen und anschließend curlen", "Bizeps", "Langsam durchführen", "https://www.youtube.com/watch?v=FtAz_85aVxE");
+        dbA.insertUebung("Squatten", "Testbeschreibung", "Testanleitung", "Testmuskelgruppe", "Testtipp", "Testvideo");
+        dbA.insertUebung("Benchpress", "Testbeschreibung", "Testanleitung", "Testmuskelgruppe", "Testtipp", "Testvideo");
+
+        Cursor c = dbA.getAllUebungen();
+
+        c.moveToFirst();
+        assertEquals("Curls", c.getString(1));
+        assertEquals("https://www.youtube.com/watch?v=FtAz_85aVxE", c.getString(6));
+
+        c.moveToNext();
+        assertEquals("Squatten", c.getString(1));
+        assertEquals("Testbeschreibung", c.getString(2));
+
+        c.moveToNext();
+        assertEquals("Benchpress", c.getString(1));
+        assertEquals("Testtipp", c.getString(5));
     }
 }
