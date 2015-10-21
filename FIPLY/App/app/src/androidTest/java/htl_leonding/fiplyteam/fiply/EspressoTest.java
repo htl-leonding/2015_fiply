1866756a3e3f5dae373eb1b5961dd2284c60877f
TurtleTower <Daniel@berdanex.net>
2015-10-21 20:37:46 +0200

    Espresso tests added

--- FIPLY/App/app/src/androidTest/java/htl_leonding/fiplyteam/fiply/EspressoTest.java
+++ FIPLY/App/app/src/androidTest/java/htl_leonding/fiplyteam/fiply/EspressoTest.java
@@ -1,21 +1,22 @@
 package htl_leonding.fiplyteam.fiply;
 
-import android.support.test.espresso.NoMatchingViewException;
-import android.support.test.espresso.ViewAssertion;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by daniel on 21.10.15.
+ * Espresso Tests
  */
 @RunWith(AndroidJUnit4.class)
 public class EspressoTest {
@@ -23,9 +24,35 @@ public class EspressoTest {
     @Rule
     public final ActivityRule<MainActivity> main = new ActivityRule<>(MainActivity.class);
 
+    //Testet, ob sich die MainActivity geöffnet ist - am Anfang - und ob die korrekte Überschrift angezeigt wird.
     @Test
-    public void test01_showcase(){
-        onView(withText("Willkommen in der FIPLY APP!")).check(ViewAssertions.matches(isDisplayed()));
+    public void test01_mainActivity(){
+        String title = getInstrumentation().getTargetContext().getResources().getString(R.string.welcome);
+        onView(withText(title)).check(ViewAssertions.matches(isDisplayed()));
+    }
+
+    //Testet, ob sich der Trainingskatalog öffnen lässt und ob die korrekte Überschrift angezeigt wird.
+    @Test
+    public void test02_katalogOeffnen(){
+        onView(withId(R.id.btStartUe)).perform(click());
+        String title = getInstrumentation().getTargetContext().getResources().getString(R.string.titleUe);
+        onView(withText(title)).check(ViewAssertions.matches(isDisplayed()));
+    }
+
+    //Testet, ob sich die Trainingssession öffnen lässt und ob die korrekte Überschrift angezeigt wird.
+    @Test
+    public void test03_trainingsSessionOeffnen() {
+        onView(withId(R.id.btStartTr)).perform(click());
+        String title = getInstrumentation().getTargetContext().getResources().getString(R.string.titleTS);
+        onView(withText(title)).check(ViewAssertions.matches(isDisplayed()));
+    }
+
+    //Testet, ob sich die Usererstellung öffnen lässt und ob die korrekte Überschrift angezeigt wird.
+    @Test
+    public void test04_userErstellungOeffnen() {
+        onView(withId(R.id.btStartEU)).perform(click());
+        String title = getInstrumentation().getTargetContext().getResources().getString(R.string.titleEU);
+        onView(withText(title)).check(ViewAssertions.matches(isDisplayed()));
     }
 
 }
