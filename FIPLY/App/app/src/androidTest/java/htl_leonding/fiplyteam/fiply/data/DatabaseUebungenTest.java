96a355b842da639609267b01da00ea15fb4c0f80
DenkerMayr <andreas.denkmayr@gmail.com>
2015-10-27 02:27:23 +0100

    Alle Tests zur KeyValueTabelle wurden hinzugefügt

--- FIPLY/App/app/src/androidTest/java/htl_leonding/fiplyteam/fiply/data/DatabaseUebungenTest.java
+++ FIPLY/App/app/src/androidTest/java/htl_leonding/fiplyteam/fiply/data/DatabaseUebungenTest.java
@@ -1,18 +1,13 @@
-package htl_leonding.fiplyteam.fiply;
+package htl_leonding.fiplyteam.fiply.data;
 
 import android.database.Cursor;
 import android.test.AndroidTestCase;
 
 import java.sql.SQLException;
 
-import htl_leonding.fiplyteam.fiply.data.UebungenRepository;
-
-public class DatabaseTest extends AndroidTestCase {
+public class DatabaseUebungenTest extends AndroidTestCase {
 
     UebungenRepository rep;
-
-    //FiplyDBHelper dbHelper = FiplyDBHelper.getInstance(mContext);
-    //SQLiteDatabase db = dbHelper.getWritableDatabase();
 
     @Override
     protected void setUp() throws Exception {
@@ -23,10 +18,13 @@ public class DatabaseTest extends AndroidTestCase {
 
     @Override
     protected void tearDown() throws Exception {
-        //FiplyDBHelper.getInstance(mContext).close();
     }
 
-    //testet das get einer Uebung mittels insert und get
+    /**
+     * testet das get einer Uebung mittels insert und get
+     *
+     * @throws SQLException
+     */
     public void testGetUebung() throws SQLException {
         Cursor c;
         rep.insertUebung("Curls", "Mit Gewichten wird gecurlt", "Gewicht nehmen und anschließend curlen", "Bizeps", "Langsam durchführen", "https://www.youtube.com/watch?v=FtAz_85aVxE");
@@ -34,7 +32,11 @@ public class DatabaseTest extends AndroidTestCase {
         assertEquals("Curls", c.getString(1));
     }
 
-    //testet das get einer Uebung über den Namen mittels insert und get
+    /**
+     * testet das get einer Uebung über den Namen mittels insert und get
+     *
+     * @throws SQLException
+     */
     public void testGetUebungByName() throws SQLException {
         Cursor c;
         rep.insertUebung("Curls", "Mit Gewichten wird gecurlt", "Gewicht nehmen und anschließend curlen", "Bizeps", "Langsam durchführen", "https://www.youtube.com/watch?v=FtAz_85aVxE");
@@ -46,7 +48,9 @@ public class DatabaseTest extends AndroidTestCase {
         assertEquals("Squatten", c.getString(1));
     }
 
-    //testet das get aller Uebungen mittels insert und getAllUebungen
+    /**
+     * testet das get aller Uebungen mittels insert und getAllUebungen
+     */
     public void testGetAllUebungen() {
         Cursor c;
         rep.insertUebung("Curls", "Mit Gewichten wird gecurlt", "Gewicht nehmen und anschließend curlen", "Bizeps", "Langsam durchführen", "https://www.youtube.com/watch?v=FtAz_85aVxE");
@@ -68,7 +72,9 @@ public class DatabaseTest extends AndroidTestCase {
         assertEquals("TestbeschreibungSquatten", c.getString(2));
     }
 
-    //testet das get aller Uebungen einer bestimmten Muskelgruppe mittels insert und getUebungenByMuskelgruppe
+    /**
+     * testet das get aller Uebungen einer bestimmten Muskelgruppe mittels insert und getUebungenByMuskelgruppe
+     */
     public void testGetUebungenByMuskelgruppe() {
         Cursor c;
         rep.insertUebung("TestBizepsC", "Mit Gewichten wird gecurlt", "Gewicht nehmen und anschließend curlen", "Bizeps", "Langsam durchführen", "https://www.youtube.com/watch?v=FtAz_85aVxE");
@@ -86,11 +92,16 @@ public class DatabaseTest extends AndroidTestCase {
         assertEquals("TestBizepsC", c.getString(1));
     }
 
-    //testet das update einer Uebung mittels insert und updateUebung
+    /**
+     * testet das update einer Uebung mittels insert und updateUebung
+     * @throws SQLException
+     */
     public void testUpdateUebung() throws SQLException {
         Cursor c;
         long updateUebungReturn;
         rep.insertUebung("Benchpress", "Testbeschreibung", "Testanleitung", "Testmuskelgruppe", "TestZIELGRUPPE", "Testvideo");
+        c = rep.getUebung(1);
+        assertEquals("Benchpress", c.getString(1));
         updateUebungReturn = rep.updateUebung(1, "Curls", "Mit Gewichten wird gecurlt", "Gewicht nehmen und anschließend curlen", "Bizeps", "Langsam durchführen", "https://www.youtube.com/watch?v=FtAz_85aVxE");
         assertEquals(1, updateUebungReturn);
         c = rep.getUebung(1);
@@ -98,7 +109,9 @@ public class DatabaseTest extends AndroidTestCase {
 
     }
 
-    //testet das count aller Uebungen mittels insert und getUebungCount
+    /**
+     * testet das count aller Uebungen mittels insert und getUebungCount
+     */
     public void testCountUebungen() {
         assertEquals(0, rep.getUebungCount());
         rep.insertUebung("Curls", "Mit Gewichten wird gecurlt", "Gewicht nehmen und anschließend curlen", "Bizeps", "Langsam durchführen", "https://www.youtube.com/watch?v=FtAz_85aVxE");
@@ -110,18 +123,23 @@ public class DatabaseTest extends AndroidTestCase {
         assertEquals(6, rep.getUebungCount());
     }
 
-    //testet das delete einer Uebung mittels insert, getUebungCount und deleteUebung
+    /**
+     * testet das delete einer Uebung mittels insert, getUebungCount und deleteUebung
+     */
     public void testDeleteUebung() {
         long deleteUebungReturn;
         rep.insertUebung("Curls", "Mit Gewichten wird gecurlt", "Gewicht nehmen und anschließend curlen", "Bizeps", "Langsam durchführen", "https://www.youtube.com/watch?v=FtAz_85aVxE");
         rep.insertUebung("Squatten", "Testbeschreibung", "Testanleitung", "Testmuskelgruppe", "TestZIELGRUPPE", "Testvideo");
-        assertEquals(2, rep.getUebungCount());
+        rep.insertUebung("Benchpress", "Testbeschreibung", "Testanleitung", "Testmuskelgruppe", "TestZIELGRUPPE", "Testvideo");
+        assertEquals(3, rep.getUebungCount());
         deleteUebungReturn = rep.deleteUebung(1);
         assertEquals(1, deleteUebungReturn);
-        assertEquals(1, rep.getUebungCount());
+        assertEquals(2, rep.getUebungCount());
     }
 
-    //testet das delete aller Uebungen mittels insert, getUebungCount und deleteAllUebungen
+    /**
+     * testet das delete aller Uebungen mittels insert, getUebungCount und deleteAllUebungen
+     */
     public void testDeleteAllUebungen() {
         long deleteAllUebungenReturn;
         assertEquals(0, rep.getUebungCount());
@@ -137,6 +155,10 @@ public class DatabaseTest extends AndroidTestCase {
         assertEquals(0, rep.getUebungCount());
     }
 
+    /**
+     * testet den Returnwert eines insert-Statements
+     * @throws SQLException
+     */
     public void testReturnOnInsert() throws SQLException {
         Cursor c;
         long insertReturn;
