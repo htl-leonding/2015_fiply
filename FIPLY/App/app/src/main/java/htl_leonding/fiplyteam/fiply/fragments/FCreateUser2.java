8ba1ff67ecafa4f658f991217141390a99a74f21
GeraldIr <gerald.irsiegler@gmail.com>
2016-01-11 12:36:50 +0100

    Create User
    Renamed Usermanagement

--- FIPLY/App/app/src/main/java/htl_leonding/fiplyteam/fiply/fragments/FCreateUser2.java
+++ FIPLY/App/app/src/main/java/htl_leonding/fiplyteam/fiply/fragments/FCreateUser2.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 
 import htl_leonding.fiplyteam.fiply.R;
 
@@ -13,6 +14,9 @@ import htl_leonding.fiplyteam.fiply.R;
  * Created by Gerildo on 10.01.2016.
  */
 public class FCreateUser2 extends Fragment {
+
+    SeekBar sbWeight;
+    SeekBar sbHeight;
 
     @Nullable
     @Override
@@ -24,6 +28,17 @@ public class FCreateUser2 extends Fragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        sbHeight = (SeekBar) getView().findViewById(R.id.sbHeight);
+        sbWeight = (SeekBar) getView().findViewById(R.id.sbWeight);
         super.onViewCreated(view, savedInstanceState);
     }
+
+    public int getHeight() {
+        return sbHeight.getProgress();
+    }
+
+    public int getWeight() {
+        return sbWeight.getProgress();
+    }
+
 }
