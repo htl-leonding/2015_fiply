e1d2a19ff6ba4a7897bfe313974439fad3f2b9d4
GeraldIr <gerald.irsiegler@gmail.com>
2016-02-03 17:45:17 +0100

    Not working in a branch
    again

--- FIPLY/App/app/src/main/java/htl_leonding/fiplyteam/fiply/fragments/FCreateUser2.java
+++ FIPLY/App/app/src/main/java/htl_leonding/fiplyteam/fiply/fragments/FCreateUser2.java
@@ -7,6 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import htl_leonding.fiplyteam.fiply.R;
 import htl_leonding.fiplyteam.fiply.data.KeyValueRepository;
@@ -18,11 +21,14 @@ public class FCreateUser2 extends Fragment {
 
     SeekBar sbWeight;
     SeekBar sbHeight;
+    TextView tvWeight;
+    TextView tvHeight;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         return inflater.inflate(R.layout.fragment_createuser2, container, false);
     }
 
@@ -33,19 +39,48 @@ public class FCreateUser2 extends Fragment {
         super.onDestroyView();
     }
 
+
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
         sbHeight = (SeekBar) getView().findViewById(R.id.sbHeight);
         sbWeight = (SeekBar) getView().findViewById(R.id.sbWeight);
+        tvHeight = (TextView) getView().findViewById(R.id.tvSizeStat);
+        tvWeight = (TextView) getView().findViewById(R.id.tvWeightStat);
+
+        sbHeight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tvHeight.setText("Größe: " + sbHeight.getProgress());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        sbWeight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tvWeight.setText("Gewicht: " + sbWeight.getProgress());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         super.onViewCreated(view, savedInstanceState);
     }
-
-    public String getHeight() {
-        return String.valueOf(sbHeight.getProgress());
-    }
-
-    public String getWeight() {
-        return String.valueOf(sbWeight.getProgress());
-    }
-
 }
