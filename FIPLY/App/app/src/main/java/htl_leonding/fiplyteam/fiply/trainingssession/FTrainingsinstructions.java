2ab5708d7d8dd5cab0c25cf83eb258a109372462
DenkerMayr <andreas.denkmayr@gmail.com>
2016-02-06 22:56:02 +0100

    Refactored unused Code in the Trainingssession

--- FIPLY/App/app/src/main/java/htl_leonding/fiplyteam/fiply/trainingssession/FTrainingsinstructions.java
+++ FIPLY/App/app/src/main/java/htl_leonding/fiplyteam/fiply/trainingssession/FTrainingsinstructions.java
@@ -11,21 +11,10 @@ import android.widget.TextView;
 
 import htl_leonding.fiplyteam.fiply.R;
 
-public class FTrainingsinstructions extends Fragment{
+public class FTrainingsinstructions extends Fragment {
 
-    String uebungsName;
-    String uebungsVideo;
-    String uebungsZG;
-    String uebungsMG;
-    String uebungsDesc;
-    String uebungsAnl;
-
-    TextView tvUebungName;
-    TextView tvUebungZG;
-    TextView tvUebungMG;
-    TextView tvUebungDesc;
-    TextView tvUebungAnl;
-    TextView tvLinkVideo;
+    String videoLink = "-";
+    TextView tvUebungName, tvUebungZG, tvUebungMG, tvUebungDesc, tvUebungAnl, tvLinkVideo;
     ImageButton ibLinkVideo;
 
     @Override
@@ -37,40 +26,35 @@ public class FTrainingsinstructions extends Fragment{
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        uebungsName = getArguments().getString("name");
-        uebungsVideo = getArguments().getString("video");
-        uebungsZG = getArguments().getString("zielgruppe");
-        uebungsMG = getArguments().getString("muskelgruppe");
-        uebungsDesc = getArguments().getString("beschreibung");
-        uebungsAnl = getArguments().getString("anleitung");
 
-        tvUebungName = (TextView) getView().findViewById(R.id.detailUebungName);
-        tvUebungMG = (TextView) getView().findViewById(R.id.detailMuskelGruppe);
-        tvUebungZG = (TextView) getView().findViewById(R.id.detailZielGruppe);
-        tvUebungDesc = (TextView) getView().findViewById(R.id.detailBeschreibung);
-        tvUebungAnl = (TextView) getView().findViewById(R.id.detailAnleitung);
-        tvLinkVideo = (TextView) getView().findViewById(R.id.tvLinkVideo);
-        ibLinkVideo = (ImageButton) getView().findViewById(R.id.ibLinkVideo);
+        tvUebungName = (TextView) getActivity().findViewById(R.id.detailUebungName);
+        tvUebungMG = (TextView) getActivity().findViewById(R.id.detailMuskelGruppe);
+        tvUebungZG = (TextView) getActivity().findViewById(R.id.detailZielGruppe);
+        tvUebungDesc = (TextView) getActivity().findViewById(R.id.detailBeschreibung);
+        tvUebungAnl = (TextView) getActivity().findViewById(R.id.detailAnleitung);
 
-        tvUebungName.setText(uebungsName);
-        tvUebungZG.setText(uebungsZG);
-        tvUebungMG.setText(uebungsMG);
-        tvUebungDesc.setText(uebungsDesc);
-        tvUebungAnl.setText(uebungsAnl);
+        tvUebungName.setText(getArguments().getString("name"));
+        tvUebungZG.setText(getArguments().getString("zielgruppe"));
+        tvUebungMG.setText(getArguments().getString("muskelgruppe"));
+        tvUebungDesc.setText(getArguments().getString("beschreibung"));
+        tvUebungAnl.setText(getArguments().getString("anleitung"));
 
-        View.OnClickListener clickVideo = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent showFullVideo = new Intent("fiply.VIDEO");
-                //Bundle extras = new Bundle();
-                //extras.putString("videoLink", "asdasdasdadadas");
-                //showFullVideo.putExtras(extras);
-                showFullVideo.putExtra("videoLink",uebungsVideo);
-                startActivity(showFullVideo);
-            }
-        };
+        videoLink = getArguments().getString("video");
+        if (videoLink != null && !videoLink.equals("-")) {
+            tvLinkVideo = (TextView) getActivity().findViewById(R.id.tvLinkVideo);
+            ibLinkVideo = (ImageButton) getActivity().findViewById(R.id.ibLinkVideo);
 
-        tvLinkVideo.setOnClickListener(clickVideo);
-        ibLinkVideo.setOnClickListener(clickVideo);
+            View.OnClickListener clickVideo = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent showFullVideo = new Intent("fiply.VIDEO");
+                    showFullVideo.putExtra("videoLink", videoLink);
+                    startActivity(showFullVideo);
+                }
+            };
+
+            tvLinkVideo.setOnClickListener(clickVideo);
+            ibLinkVideo.setOnClickListener(clickVideo);
+        }
     }
 }
