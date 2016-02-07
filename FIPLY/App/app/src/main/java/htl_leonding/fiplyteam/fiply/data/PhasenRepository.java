af9266014f4927b4ed1aa4b97e9cfbe1a60eefa7
TurtleTower <Daniel@berdanex.net>
2016-02-07 15:22:32 +0100

    Phasenrepo added

--- FIPLY/App/app/src/main/java/htl_leonding/fiplyteam/fiply/data/PhasenRepository.java
+++ FIPLY/App/app/src/main/java/htl_leonding/fiplyteam/fiply/data/PhasenRepository.java
@@ -1,8 +1,11 @@
 package htl_leonding.fiplyteam.fiply.data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
+import htl_leonding.fiplyteam.fiply.data.FiplyContract.PhasenEntry;
+
 
 public class PhasenRepository {
 
@@ -21,7 +24,7 @@ public class PhasenRepository {
 
     private SQLiteDatabase getWritableDatabase() {
         if (repoContext == null)
-            throw new IllegalStateException("Context is null - " + "Set a repoContext in the Repository with setContext()");
+            throw new IllegalStateException("Context is null - Set a repoContext in the Repository with setContext()");
 
         Log.wtf(FiplyDBHelper.LOG_TAG, "Repository::getWriteableDatabase()");
         return FiplyDBHelper.getInstance(repoContext).getWritableDatabase();
@@ -29,11 +32,23 @@ public class PhasenRepository {
 
     private SQLiteDatabase getReadableDatabase() {
         if (repoContext == null)
-            throw new IllegalStateException("Context is null - " + "Set a repoContext in the Repository with setContext()");
+            throw new IllegalStateException("Context is null - Set a repoContext in the Repository with setContext()");
 
         Log.wtf(FiplyDBHelper.LOG_TAG, "Repository::getReadableDatabase()");
         return FiplyDBHelper.getInstance(repoContext).getReadableDatabase();
     }
 
 
+    public long insertPhase(String startDate, String endDate, String phasenName,
+                             String phasenDauer, String pausenDauer, String saetze, String wiederholungen){
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(PhasenEntry.COLUMN_STARTDATE, startDate);
+        initialValues.put(PhasenEntry.COLUMN_ENDDATE, endDate);
+        initialValues.put(PhasenEntry.COLUMN_PHASENNAME, phasenName);
+        initialValues.put(PhasenEntry.COLUMN_PHASENDAUER, phasenDauer);
+        initialValues.put(PhasenEntry.COLUMN_PAUSENDAUER, pausenDauer);
+        initialValues.put(PhasenEntry.COLUMN_SAETZE, saetze);
+        initialValues.put(PhasenEntry.COLUMN_WIEDERHOLUNGEN, wiederholungen);
+        return db.insert(PhasenEntry.TABLE_NAME, null, initialValues);
+    }
 }
