9dfb0f345221e307368c7e83a42caaeca309e8a3
DenkerMayr <andreas.denkmayr@gmail.com>
2016-02-27 20:16:16 +0100

    Added Settings

--- FIPLY/App/app/src/main/java/htl_leonding/fiplyteam/fiply/menu/FAppSettings.java
+++ FIPLY/App/app/src/main/java/htl_leonding/fiplyteam/fiply/menu/FAppSettings.java
@@ -1,15 +1,29 @@
 package htl_leonding.fiplyteam.fiply.menu;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.preference.PreferenceFragmentCompat;
 
 import htl_leonding.fiplyteam.fiply.R;
 
-public class FAppSettings extends PreferenceFragment{
+public class FAppSettings extends PreferenceFragmentCompat {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
 
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getFragmentManager().beginTransaction().hide(FAppSettings.this).commit();
+        getFragmentManager().beginTransaction().replace(R.id.fraPlace, new FMain()).commit();
     }
 }
--- FIPLY/App/app/src/main/java/htl_leonding/fiplyteam/fiply/menu/FMain.java
+++ FIPLY/App/app/src/main/java/htl_leonding/fiplyteam/fiply/menu/FMain.java
@@ -137,45 +137,19 @@ public class FMain extends Fragment {
         startSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*
-                FSettings fSettings = new FSettings();
-                displayView(fSettings);
-                */
-                notifyUser();
+//                FSettings fSettings = new FSettings();
+//                displayView(fSettings);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.fraPlace, new FAppSettings())
+                        .addToBackStack(null)
+                        .commit();
             }
         });
 
+
+
+
         //getActivity().startService(new Intent(getActivity(), NotificationService.class));
-    }
-
-    private void notifyUser() {
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(getActivity())
-                        .setSmallIcon(R.drawable.splash1)
-                        .setContentTitle("FIPLY")
-                        .setContentText("Du hast für heute ein Training geplant!");
-// Creates an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(getActivity(), SplashActivity.class);
-
-// The stack builder object will contain an artificial back stack for the
-// started Activity.
-// This ensures that navigating backward from the Activity leads out of
-// your application to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(getActivity());
-// Adds the back stack for the Intent (but not the Intent itself)
-        stackBuilder.addParentStack(SplashActivity.class);
-// Adds the Intent that starts the Activity to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(
-                        0,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        mBuilder.setContentIntent(resultPendingIntent);
-        NotificationManager mNotificationManager =
-                (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
-// mId allows you to update the notification later on.
-        mNotificationManager.notify(123, mBuilder.build());
     }
 
     /**
